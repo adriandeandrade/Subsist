32f10d4f1a1958ac051247237148a11c1ce4ff97
Adrian De Andrade <redfoxhint4@hotmail.com>
2017-06-27 20:45:25 -0400

    Stuff
    Player controller and camera controller

---
+++ Subsist/Assets/Scripts/Player/ThirdPersonCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public float speed = 1f;
+    public Transform target;
+    public Camera cam;
+
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        Vector3 direction = (target.position - cam.transform.position).normalized;
+
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        lookRotation.x = transform.rotation.x;
+        lookRotation.y = transform.rotation.y;
+        lookRotation.z = transform.rotation.z;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 100);
+
+        transform.position = Vector3.Slerp(transform.position, target.position + offset, Time.deltaTime * speed);
+    }
+}
